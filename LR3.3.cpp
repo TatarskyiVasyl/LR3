0dd27b629bb5100238c40495e37cf51cf87212a5
TatarskyiVasyl <tatarskyi.vasyl.kn@dspu.edu.ua>
2025-10-13 15:06:41 +0300

    Update LR3.3.cpp

--- LR3.3.cpp
+++ LR3.3.cpp
@@ -1,28 +1,40 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cmath>
 #include <Windows.h>
 using namespace std;
 
 int main()
 {
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
 
     double R, x, y;
     cout << "R = "; cin >> R;
     cout << "x = "; cin >> x;
 
-    if (x <= -8)
+    if (x <= -8 - R)
+        // горизонтальна лінія зліва
         y = 0;
-    else if (x > -8 && x <= -R)
-        y = (R / (8 - R)) * (x + 8);
-    else if (x > -R && x < R)
-        y = -sqrt(R * R - x * x);
-    else if (x >= R && x <= 5)
-        y = ((3 - R) / (5 - R)) * (x - R) + R;
-    else // x > 5
-        y = 3;
+    else if (x > -8 - R && x <= -8 + R)
+        // верхня півокруга з центром у (-8, 0)
+        y = sqrt(R * R - pow(x + 8, 2));
+    else if (x > -8 + R && x <= 0)
+        // пряма лінія, що з'єднує (-8+R, -R) і (0, 2)
+        y = ((2 - (-R)) / (0 - (-8 + R))) * (x - (-8 + R)) + (-R);
+    else if (x > 0 && x <= 2)
+        // лінія від (0,0) до (2,2)
+        y = x;
+    else if (x > 2 && x <= 6)
+        // відрізок на осі X
+        y = 0;
+    else if (x > 6 && x <= 8)
+        // парабола y = (x - 6)^2
+        y = pow(x - 6, 2);
+    else
+        // за межами графіка
+        y = 0;
 
     cout << "y = " << y << endl;
+
     return 0;
 }
