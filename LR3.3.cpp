6f0d515bed6100bc54ee619779d49fd38f390770
TatarskyiVasyl <tatarskyi.vasyl.kn@dspu.edu.ua>
2025-10-16 20:12:08 +0300

    Update LR3.3.cpp

--- LR3.3.cpp
+++ LR3.3.cpp
@@ -4,40 +4,46 @@
 // Розгалуження, задане графіком функції
 // Варіант 28
 
+// Lab_03_3.cpp
+// Татарський Василь Петрович
+// Лабораторна робота №3.3
+// Розгалуження, задане графіком функції
+// Варіант 28
+
 #include <iostream>
 #include <cmath>
 #include <Windows.h>
-using namespace std;
 
+using namespace std;
 int main()
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-    double x, y, R;
+    double x; // вхідний аргумент
+    double R; // вхідний параметр
+    double y; // результат обчислення виразу
 
-    cout << "Введіть x, R: ";
-    cin >> x >> R;
+    cout << "R = "; cin >> R;
+    cout << "x = "; cin >> x;
 
-    // Розгалуження у повній формі
+    // розгалуження в повній формі 
     if (x <= -8 - R)
-        y = 0;
-    else 
-        if (-8 - R < x && x <= -8 + R)
-        y = -R + sqrt(R*R - (x + 8)*(x + 8));   // півколо
-    else 
-            if (-8 + R < x && x <= 2)
-        y = 1.0 * x + 2;                            // пряма, через (0;2)
-    else 
-                if (2 < x && x < 6)
-        y = 0;                                      // проміжок по осі X
-    else 
-                    if (6 <= x && x <= 8)
-        y = (x - 6)*(x - 6);                          // парабола
+        y = -R;
     else
-        y = 0;
+        if (-8 - R <= x && x <= -8 + R)
+            y = -R + sqrt(R * R - (x + 8) * (x + 8));
+        else
+            if (-8 + R <= x && x <= 2)
+                y = (((x + 8 - R) * (R + 2)) / (10 - R)) - R;
+            else
+                if (2 <= x && x <= 6)
+                    y = 0;
+                else
+                    y = (x - 6) * (x - 6);
 
+    cout << endl;
     cout << "y = " << y << endl;
-
+    cin.get();
     return 0;
 }
